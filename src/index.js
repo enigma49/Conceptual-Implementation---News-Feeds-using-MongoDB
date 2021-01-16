a533d4f950effc9eee0cecd5ba54b006ae93821b
Hamza Shaikh <shaikhhamza157@gmail.com>
2021-01-16 17:55:45 +0530

    aquib fixed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const newsArticleModel = require('./connector');
+const { newsArticleModel } = require('./connector')
+
+const onePageArticleCount = 10
 
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const onePageArticleCount = 10
-// app.get("/newFeeds", async function(req,res){
-//     try{
-//         let limit = (isNaN(req.query.limit) || null || undefined || req.query.limit < 0) ? 10 : req.query.limit;
-//         let offset = (isNaN(req.query.offset) || null || undefined || req.query.offset < 0) ? 0 : req.query.offset;
-//         let data = await newsArticleModel.find().skip(Number(offset)).limit(Number(limit));
-//         res.send(data);
-//     }catch(error){
-//         res.send(error.message);
-//     }
-// });
 
 app.get("/newFeeds",async (req,res)=>{
     let limit = Number(req.query.limit);
